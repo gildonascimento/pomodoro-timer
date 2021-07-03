db3f0f14c2e09e52a6ea4559bb2344c672972285
gildonascimento <bcgildo@gmail.com>
2021-07-02 21:24:43 -0400

    Timer

--- main.js
+++ main.js
@@ -8,3 +8,36 @@ function corpo() {
   document.getElementById("minutos").innerHTML = minutos;
   document.getElementById("segundos").innerHTML = segundos;
 }
+
+function start_timer() {
+  play.play();
+
+  minutos = 24;
+  segundos = 59;
+
+  document.getElementById("minutos").innerHTML = minutos;
+  document.getElementById("segundos").innerHTML = segundos;
+
+  var minutos_interval = setInterval(minutosTempo, 60000);
+  var segundos_interval = setInterval(segundosTempo, 1000);
+
+  function minutosTempo() {
+    minutos = minutos - 1;
+    document.getElementById("minutos").innerHTML = minutos;
+  }
+
+  function segundosTempo() {
+    segundos = segundos - 1;
+    document.getElementById("segundos").innerHTML = segundos;
+
+    if (segundos <= 0) {
+      if (minutos <= 0) {
+        clearInterval(minutos_interval);
+        clearInterval(segundos_interval);
+
+        sino.play();
+        window.alert("Ciclo completo, faça um intervalo!");
+      }
+    }
+  }
+}
